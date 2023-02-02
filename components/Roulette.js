729629015fb7c37334b96b37a9e151ad5ee0c32f
anuj <anujgupta@fynd-external.com>
2023-02-02 20:14:39 +0530

    done

--- components/Roulette.js
+++ components/Roulette.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import styles from '../styles/Roulette.module.css';
 export const Roulette = () => {
   const [memberList, setMember] = useState([]);
@@ -6,8 +6,11 @@ export const Roulette = () => {
   const addMember = (e) => {
     e.preventDefault();
     setMember(memberList => [...memberList, e.target[0].value])
-    console.log(memberList);
   }
+  useEffect(() => {
+    inputRef.current.value = "";
+  })
+  const inputRef = useRef(null)
   const handleClick = (e) => {
     const randomElement = memberList[Math.floor(Math.random() * memberList.length)];
     setWinner(randomElement);
@@ -19,12 +22,13 @@ export const Roulette = () => {
   return (
     <div style={setBackground()}>
       <h1 className={styles.title}>Welcome to Chai Roulette Boys</h1>
+      <h6 className={styles.title}>(Because girls will never do this stupid shit )</h6>
       <h3 className={styles.title}>Please enter member names</h3>
       <form className={styles.center} onSubmit={addMember}>
-        <input type="text" />
+        <input type="text" ref={inputRef} />
         <button type="submit">Add member</button>
       </form>
-      <div className={styles.center}>Currently added members are {memberList.map(el => { return <div key={el}>{el}</div> })} </div>
+      {memberList.length > 0 && <div className={[styles.center, styles.memberInfo].join(' ')}>Currently added members are <ol> {memberList.map(el => { return <li key={el}>{el}</li> })} </ol> </div>}
       <div className={styles.center}><button onClick={handleClick}>Select Payer</button></div>
       {winner && <h1 className={styles.title}>Congrats {winner}, pay up now</h1>}
     </div>
