b0d64c315f45b33d10a588194adb30ff09a4c7a5
anuj <anujgupta@fynd-external.com>
2023-02-02 19:29:37 +0530

    added color changing on submit

--- components/Roulette.js
+++ components/Roulette.js
@@ -1,29 +1,33 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/Roulette.module.css';
 export const Roulette = () => {
-const [memberList,setMember] = useState([]);
-const [winner,setWinner] = useState();
-const addMember = (e)=>{
-  e.preventDefault();
-  setMember(memberList=> [...memberList,e.target[0].value])
-  console.log(memberList);
-} 
-const handleClick = (e)=>{
-  const randomElement = memberList[Math.floor(Math.random() * memberList.length)];
-setWinner(randomElement);
-}
+  const [memberList, setMember] = useState([]);
+  const [winner, setWinner] = useState();
+  const addMember = (e) => {
+    e.preventDefault();
+    setMember(memberList => [...memberList, e.target[0].value])
+    console.log(memberList);
+  }
+  const handleClick = (e) => {
+    const randomElement = memberList[Math.floor(Math.random() * memberList.length)];
+    setWinner(randomElement);
+  }
+  const setBackground = () => {
+    var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    return { background: '#' + randomColor }
+  }
   return (
-    <>
+    <div style={setBackground()}>
       <h1 className={styles.title}>Welcome to Chai Roulette Boys</h1>
       <h3 className={styles.title}>Please enter member names</h3>
-      <form onSubmit={addMember}>
+      <form className={styles.center} onSubmit={addMember}>
         <input type="text" />
         <button type="submit">Add member</button>
       </form>
-      <div>Currently added members are {memberList.map(el=>{return <div key={el}>{el}</div>})} </div>
-      <button onClick={handleClick}>Select Payer</button>
-      {winner && <h1>{winner}</h1>}
-    </>
+      <div className={styles.center}>Currently added members are {memberList.map(el => { return <div key={el}>{el}</div> })} </div>
+      <div className={styles.center}><button onClick={handleClick}>Select Payer</button></div>
+      {winner && <h1 className={styles.title}>Congrats {winner}, pay up now</h1>}
+    </div>
   )
 }
 
