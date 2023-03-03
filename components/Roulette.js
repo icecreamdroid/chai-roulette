7de8b7f9e44e61a2cf5f19522ae5cc4d532f9f85
anuj <anujgupta@fynd-external.com>
2023-03-03 17:55:33 +0530

    Loader added

--- components/Roulette.js
+++ components/Roulette.js
@@ -1,8 +1,10 @@
 import React, { useState, useRef, useEffect } from 'react'
 import styles from '../styles/Roulette.module.css';
+import Spinner from './Spinner';
 export const Roulette = () => {
   const [memberList, setMember] = useState([]);
   const [winner, setWinner] = useState();
+  const [on , setSpinner] = useState(1);
   const addMember = (e) => {
     e.preventDefault();
     setMember(memberList => [...memberList, e.target[0].value])
@@ -14,15 +16,15 @@ export const Roulette = () => {
   const handleClick = (e) => {
     const randomElement = memberList[Math.floor(Math.random() * memberList.length)];
     setWinner(randomElement);
+    setSpinner(on+1);
   }
   const setBackground = () => {
     var randomColor = Math.floor(Math.random() * 16777215).toString(16);
     return { background: '#' + randomColor }
   }
   return (
-    <div style={setBackground()}>
+    <div className={styles.background} style={setBackground()}>
       <h1 className={styles.title}>Welcome to Chai Roulette Boys</h1>
-      <h6 className={styles.title}>(Because girls will never do this stupid shit )</h6>
       <h3 className={styles.title}>Please enter member names</h3>
       <form className={styles.center} onSubmit={addMember}>
         <input type="text" ref={inputRef} />
@@ -30,7 +32,9 @@ export const Roulette = () => {
       </form>
       {memberList.length > 0 && <div className={[styles.center, styles.memberInfo].join(' ')}>Currently added members are <ol> {memberList.map(el => { return <li key={el}>{el}</li> })} </ol> </div>}
       <div className={styles.center}><button onClick={handleClick}>Select Payer</button></div>
-      {winner && <h1 className={styles.title}>Congrats {winner}, pay up now</h1>}
+      {/* {winner && <h1 className={styles.title}>Congrats {winner}, pay up now</h1>} */}
+
+      {winner && <Spinner on={on} names={memberList} winner={winner}></Spinner>}
     </div>
   )
 }
---
+++ components/Spinner.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import styles from '../styles/Spinner.module.css';
+import { useEffect, useState } from 'react'
+import Image from 'next/image';
+function Spinner(props) {
+    const state = {
+        nextRandomColor: Math.floor(Math.random(0) * 16777215).toString(16).slice(0, 3),
+    }
+    const [keyNumber, setKeyNumber] = useState(1);
+    const [colorKey,setColorKey] = useState(1);
+    const [delay, reduceDelay] = useState(5000)
+    const [rotation,setRotation] = useState(1)
+    useEffect(() => {
+
+        const timer = setInterval(() => {
+            if (delay >= 0) {
+                setKeyNumber(keyNumber + 1);
+                reduceDelay(delay - 100)
+            }
+            if(delay<0){
+                setColorKey(colorKey+100);
+                setRotation(rotation+10);
+            }
+        }, 50);
+       
+
+
+        return () => clearInterval(timer);
+    })
+    useEffect(()=>{
+        reduceDelay(5000);
+    },[props.on])
+
+    const setBackground = () => {
+        var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+        return {
+            background: 'radial-gradient(#' + randomColor + ','
+                + '#' + state.nextRandomColor + keyNumber + ')'
+        }
+    }
+     const setColor = () => {
+        var randomColorText = Math.floor(Math.random() * 16777215).toString(16);
+
+        return {
+            color: '#' + (randomColorText + colorKey)
+        }
+    }
+    const rotateShit = ()=>{
+        
+        return {transform:`rotate(${rotation}deg)`}
+    }
+    return (
+        <>
+            <div style={setBackground()} className={styles.spinner}>
+            {props.winner && props.on && delay<=0 && <div className={styles.winner}> <Image style={rotateShit()} src={require('../svgone.svg')} width="100" height="100" ></Image> </div>}
+            {props.winner && props.on && delay<=0 && <div style={setColor()} className={styles.winner}> {props.winner}</div>}
+            {props.on && props.names && delay>0 && <div  className={`${styles.winner} ${styles.names}`}>Not {props.names[Math.floor(Math.random() * props.names.length)]}  </div>}
+
+
+            </div>
+        </>
+    )
+}
+
+export default Spinner
